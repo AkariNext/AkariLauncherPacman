fd05e8471f5e41f66579ae5a5d22f5c1c4dc79b1
yupix <yupi0982@outlook.jp>
2024-04-03 21:55:34 +0900

    feat: DistributionManagerの中身を実装

--- src/distribution.py
+++ src/distribution.py
@@ -1,6 +1,34 @@
+import json
+from src.types import IDistribution
+from src.utils.common import load_or_create_json
+from src.utils.read import Read
+
+
 class DistributionManager:
     def __init__(self) -> None:
-        pass
+        self.distribution:IDistribution = load_or_create_json('./distribution.json')  # type: ignore
 
-    async def gets(self):
-        ...
+    async def init(self):
+        name = Read('リポジトリ名を入力してください').read().value
+        repository_type = Read('リポジトリの種類を指定してください').enum(['s3', 'github']).read().value
+        base_url = Read('ベースURLを指定してください。GitHubの場合はrawのブランチを指定').read().value
+        self.distribution = {  # type: ignore
+            'name': name,
+            'modpacks': [],
+            'repository_type': repository_type, # type: ignore
+            'format_version': '1.0.0',
+            'base_url': base_url
+        }
+        await self.save()
+
+    async def add_modpack(self, name: str):
+        """ModPackを追加します"""
+        self.distribution['modpacks'].append(name)
+
+    async def get_modpacks(self):
+        """存在するModPackの一覧を返します"""
+        return self.distribution['modpacks']
+
+    async def save(self):
+        with open('./distribution.json', mode='w', encoding='utf-8') as f:
+            json.dump(self.distribution, f, ensure_ascii=False, indent=4)
