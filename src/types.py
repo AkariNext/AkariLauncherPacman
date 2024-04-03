fab53e4fa27b8320fe0c92518255fbba77669424
yupix <yupi0982@outlook.jp>
2024-04-03 21:55:09 +0900

    chore: 型を変更

--- src/types.py
+++ src/types.py
@@ -7,7 +7,7 @@ class IManifestServer(TypedDict):
 
 
 class IManifestModLoader(TypedDict):
-    modLoaderType: Literal["forge", "fabric"]
+    modLoaderType: str
     modLoaderVersion: str
 
 
@@ -23,4 +23,5 @@ class IDistribution(TypedDict):
     modpacks: list[str]
     repository_type: Literal['github', 's3']
     format_version: str
+    base_url: str
 
